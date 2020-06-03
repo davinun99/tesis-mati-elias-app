8f8857f3b8efe359eee6c497adb6e134e31befc8
Matias <matias.rt@hotmail.com>
2020-06-02 22:03:18 -0400

    Eliminadas pantallas y funciones que no se utilizaran

--- portalocdspy_backend/urls.py
+++ portalocdspy_backend/urls.py
@@ -19,31 +19,6 @@ urlpatterns = [
 	path('inicio/', viewsets.Index.as_view()),
 	path('buscador/', viewsets.Buscador.as_view()),
 
-	path('proveedores/', viewsets.Proveedores.as_view()),
-	path('proveedores/<path:partieId>/productos/', viewsets.ProductosDelProveedor.as_view()),
-	path('proveedores/<path:partieId>/contratos/', viewsets.ContratosDelProveedor.as_view()),
-	path('proveedores/<path:partieId>/pagos/', viewsets.PagosDelProveedor.as_view()),
-	path('proveedores/<path:partieId>/', viewsets.Proveedor.as_view()),
-	
-	path('proveedores-sefin/', viewsets.ProveedoresSEFIN.as_view()),
-	path('proveedores-sefin/filtros/', viewsets.FiltroAniosProveedoresSEFIN.as_view()),
-
-	path('compradores/', viewsets.Compradores.as_view()),
-	path('compradores/<path:partieId>/procesos/', viewsets.ProcesosDelComprador.as_view()),
-	path('compradores/<path:partieId>/contratos/', viewsets.ContratosDelComprador.as_view()),
-	path('compradores/<path:partieId>/pagos/', viewsets.PagosDelComprador.as_view()),
-	path('compradores/<path:partieId>/', viewsets.Comprador.as_view()),
-
-	path('dashboardsefin/filtros/', viewsets.FiltrosDashboardSEFIN.as_view()),
-	path('dashboardsefin/cantidaddepagos/', viewsets.GraficarCantidadDePagosMes.as_view()),
-	path('dashboardsefin/montosdepagos/', viewsets.GraficarMontosDePagosMes.as_view()),
-	path('dashboardsefin/estadisticamontosdepagos/', viewsets.EstadisticaMontoDePagos.as_view()),
-	path('dashboardsefin/estadisticacantidaddepagos/', viewsets.EstadisticaCantidadDePagos.as_view()),
-	path('dashboardsefin/topcompradores/', viewsets.TopCompradoresPorMontoPagado.as_view()),
-	path('dashboardsefin/topproveedores/', viewsets.TopProveedoresPorMontoPagado.as_view()),
-	path('dashboardsefin/topobjetosgasto/', viewsets.TopObjetosDeGastoPorMontoPagado.as_view()),
-	path('dashboardsefin/etapaspago/', viewsets.EtapasPagoProcesoDeCompra.as_view()),
-
 	path('dashboardoncae/filtros/', viewsets.FiltrosDashboardONCAE.as_view()),
 	path('dashboardoncae/procesosporcategoria/', viewsets.GraficarProcesosPorCategorias.as_view()),
 	path('dashboardoncae/procesospormodalidad/', viewsets.GraficarProcesosPorModalidad.as_view()),
